9e25fe6b871acc51d06cf7bb4440e0272e3501ab
coderkatew <kateat20@yahoo.co.uk>
2020-12-21 11:49:09 +0000

    Add edit product funcionality

--- products/views.py
+++ products/views.py
@@ -69,3 +69,26 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+    """ Edit existing product  """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Cannot update this product. Please ensure form is complete.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
