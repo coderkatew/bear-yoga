1b79aa0acd4dc35afcda7149f90047125069cabd
coderkatew <kateat20@yahoo.co.uk>
2020-12-28 19:34:06 +0000

    Change order of list in context to address bug

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,14 +22,14 @@ def bag_contents(request):
                 'product': product,
             })
 
-            if total < settings.FREE_DELIVERY_THRESHOLD:
-                delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-                free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-            else:
-                delivery = 0
-                free_delivery_delta = 0
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
 
-            grand_total = delivery + total
+    grand_total = delivery + total
             
 
     context = {
@@ -37,9 +37,9 @@ def bag_contents(request):
         'order_total': total,
         'item_count': product_count,
         'delivery': delivery,
-        'grand_total': grand_total,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
