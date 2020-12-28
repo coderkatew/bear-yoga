cbf7aa1daa4385755ad8ac27ee7d27a888c79724
coderkatew <kateat20@yahoo.co.uk>
2020-12-28 17:58:29 +0000

    Update retreats model with duration

--- retreats/models.py
+++ retreats/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Retreat(models.Model):
     name = models.CharField(max_length=200)
     date = models.CharField(max_length=200)
-    duration = models.DurationField()
+    duration = models.CharField(max_length=200)
     description = models.TextField(null=False, blank=False, default='')
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     location = models.CharField(max_length=200)
