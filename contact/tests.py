d7fb58c2c04931390e066953e009549926615e92
coderkatew <kateat20@yahoo.co.uk>
2020-12-29 11:22:49 +0000

    Add tests for contact app form

--- contact/tests.py
+++ contact/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .forms import ContactForm
 
-# Create your tests here.
+
+class TestContactForm(TestCase):
+
+    def test_required_fields(self):
+        form = ContactForm({
+            'name': 'John Doe',
+            'email': 'bearyoga@example.com',
+            'message': 'Hello, World.'})
+        self.assertFalse(form.errors)
+
+    def test_alert_invalid_form(self):
+        form = ContactForm({'form': ''})
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
