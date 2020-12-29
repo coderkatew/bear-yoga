b5b45029e56589d9bffc28d5ec182d074b5281e6
coderkatew <kateat20@yahoo.co.uk>
2020-12-29 11:49:57 +0000

    Update database settings and add comment for saved app test

--- bear_yoga/settings.py
+++ bear_yoga/settings.py
@@ -122,18 +122,17 @@ WSGI_APPLICATION = 'bear_yoga.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
     }
 
 # Password validation
--- saved/tests.py
+++ saved/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+# Simple test to check page exists
 def test_Saved_get(self):
     resp = self.client.get(reverse('saved:saved'))
     self.assertEqual(resp.status_code, 302)
