8a1f214534a82421d2e493901661e5d264e3ffe0
coderkatew <kateat20@yahoo.co.uk>
2020-12-29 11:42:16 +0000

    Add page test for saved app

--- saved/tests.py
+++ saved/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+def test_Saved_get(self):
+    resp = self.client.get(reverse('saved:saved'))
+    self.assertEqual(resp.status_code, 302)
+    self.assertRedirects(resp, "/saved/")
