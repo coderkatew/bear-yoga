b72f528ad4ba9a7a3eeea19e44d4997a09f23e87
coderkatew <kateat20@yahoo.co.uk>
2020-12-29 11:34:29 +0000

    Add tests for retreats form

--- retreats/tests.py
+++ retreats/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .forms import RetreatForm
 
-# Create your tests here.
+
+class TestRetreatForm(TestCase):
+
+    def test_required_fields(self):
+        form = RetreatForm({
+            'name': 'Yoga Retreat',
+            'date': '06/10/2021',
+            'duration': '3',            
+            'description': 'Lovely retreat.',
+            'price': '150',
+            'location': 'Italy'})
+        self.assertFalse(form.errors)
+
+    def test_alert_invalid_form(self):
+        form = RetreatForm({'form': ''})
+        self.assertEqual(form.errors['location'], [u'This field is required.'])
